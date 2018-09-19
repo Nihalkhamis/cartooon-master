ffc097e1e317e461ca2a97fb362737b6557c400e
Nihal Yasser <nihalyasser23@gmail.com>
2018-09-19 02:11:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dell/cartoon/timonandpumba.java
@@ -0,0 +1,18 @@
+package com.example.dell.cartoon;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class timonandpumba extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_timonandpumbba , container , false);
+    }
+}
